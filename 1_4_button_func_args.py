bab0a485bf789f537a61807582e6345806d387ef
Shallmore <105773995+shallmore@users.noreply.github.com>
2023-09-29 02:08:50 +0900

    Update 1_4_button_func_args.py
    that is to made some modification.

--- 1_4_button_func_args.py
+++ 1_4_button_func_args.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import ttkbootstrap as ttk
 
 def button_func(entry_string):
-	print('a button was pressed')
+	# print('a button was pressed')
 	print(entry_string.get())
 
 def outer_func(parameter):
@@ -16,7 +16,7 @@ window = ttk.Window()
 window.title('buttons, functions and arguments')
 
 # widgets 
-entry_string = tk.StringVar(value = 'test')
+entry_string = tk.StringVar(value = 'here')
 entry = ttk.Entry(window, textvariable = entry_string)
 entry.pack()
 
